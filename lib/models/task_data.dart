c6595d35d0ae79a3dd38423542b4b88d13f31568
Dmitriy K <fobfreeman@gmail.com>
2021-10-04 16:04:38 +0300

    check,read and add tasks

---
+++ lib/models/task_data.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:todoey/models/task.dart';
+import 'package:flutter/foundation.dart';
+import 'package:todoey/models/task.dart';
+import 'dart:collection';
+
+class TaskData extends ChangeNotifier {
+  final List<Task> _tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
+  UnmodifiableListView get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  int get taskCount {
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskTitle) {
+    final task = Task(name: newTaskTitle);
+    _tasks.add(task);
+    notifyListeners();
+  }
+}
