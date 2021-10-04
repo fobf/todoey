dcf9fdd451067d16832dd9d922810650c43bc49b
Dmitriy K <fobfreeman@gmail.com>
2021-10-04 16:55:09 +0300

    clean up

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todoey/models/task_data.dart';
-import 'package:todoey/models/task.dart';
 
 // ignore: use_key_in_widget_constructors
 class AddTaskScreen extends StatelessWidget {
--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todoey/widgets/tasks_list.dart';
 import 'package:todoey/screens/add_task_screen.dart';
-import 'package:provider/provider.dart';
+
 import 'package:todoey/models/task_data.dart';
 
 // ignore: use_key_in_widget_constructors
